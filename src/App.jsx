d228ba34444974552cd6e43721f01f5795a14b76
Awais <159285589+mawaisakhter@users.noreply.github.com>
2024-09-29 20:55:58 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import Home from './Home/Home';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer/Footer';
 import About from './Pages/About/About';
-import Register from './Forms/Register';
-import ContactUs from './Pages/ContactUs';
+import Register from './Pages/Forms/Register';
+import ContactUs from './Pages/Forms/ContactUs';
+import WomenCategoryPage from './Home/Sidebar/Category/WomenCategoryPage';
+import MenCategoryPage from './Home/Sidebar/Category/MenCategoryPage';
+import CategoryPage from './Home/Sidebar/Category/CategoryPage';
+import Cart from './Pages/Cart/Cart';
+import Wishlist from './Pages/Wishlist/Wishlist';
+import Login from './Pages/Forms/Login';
 
 
 const App = () => {
@@ -14,9 +20,15 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/contact" element={<ContactUs/>}/>
-        <Route path="/about" element={<About/>} />
-        {/* <Route path="/account/register" element={<Register/>} /> */}
+        <Route path="contact" element={<ContactUs/>}/>
+        <Route path="about" element={<About/>} />
+        <Route path="account/register" element={<Register/>} />
+        <Route path="account/login" element={<Login/>} />
+        <Route path="account/cart" element={<Cart/>} />
+        <Route path="account/wishlist" element={<Wishlist/>} />
+        <Route path="category/women/:category" element={<CategoryPage/>} />
+        <Route path="category/men/:category" element={<CategoryPage/>} />
+        <Route path="category/:category" element={<CategoryPage/>} />
       </Routes>
       <Footer/>
     </Router>
